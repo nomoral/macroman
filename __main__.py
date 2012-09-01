2c52494967204d362ee1d1f74679524044f6a51b
Irae <iraehc@hotmail.com>
2012-09-01 18:18:52 +0200

    added copyright notice

--- __main__.py
+++ __main__.py
@@ -1,2 +1,23 @@
+#!/usr/bin/python
+
+
+# macroman, a build tool for HTML5 app
+#   Copyright (C) 2012  Irae Hueck Costa
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 3 of the License, or
+#   (at your option) any later version.
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, write to the Free Software Foundation,
+#   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+
+
+
+
+
 from macroman import main
 main()
